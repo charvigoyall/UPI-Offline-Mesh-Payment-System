c15e08b7cdbc164e20e7542f1eff1301fd9ae18f
charvigoyall <charvigoyal444@gmail.com>
2026-08-14 23:55:15 +0530

    Update BridgeIngestionService.java

--- src/main/java/com/demo/upimesh/service/BridgeIngestionService.java
+++ src/main/java/com/demo/upimesh/service/BridgeIngestionService.java
@@ -88,4 +88,4 @@ public class BridgeIngestionService {
          public static IngestResult invalid(String hash, String reason) {
             return new IngestResult("INVALID", hash, reason, null);
     }
-}//
+}
